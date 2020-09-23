526cbcb305fd1e5e05d1d45e1b9bb82c76b5e428
fedecascardo <fedecasca@gmail.com>
2020-09-23 19:24:03 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -18,20 +18,28 @@ self.addEventListener('activate', e=> {
     //Actualizar la caché. Realizar cambios necesarios en nuevas versiones de cache
 })
 
-self.addEventListener('fetch', e=>{
-    //Responder priorizando la caché por sobre la red - cache first
-    //Intercepta una petición http
-    //console.log('SW fetch');  
-    //let request =  e.request;
-    //console.log(request.url)
-    //let response;
-    //Esto sería el comportamiento por defecto. Responder con e.request
-    //e.respondWith(fetch(e.request));
-    console.log(e.request.url)
-    const respuesta = caches.open('CACHE_APP_LOTES').then(
-        cache => {
-            caches.match(e.request.url)
-        }
-    )
-    e.respondWith(respuesta);
-})
+// self.addEventListener('fetch', e=>{
+//     //Responder priorizando la caché por sobre la red - cache first
+//     //Intercepta una petición http
+//     //console.log('SW fetch');  
+//     //let request =  e.request;
+//     //console.log(request.url)
+//     //let response;
+//     //Esto sería el comportamiento por defecto. Responder con e.request
+//     //e.respondWith(fetch(e.request));
+//     console.log(e.request.url)
+//     const respuesta = caches.open('CACHE_APP_LOTES').then(
+//         cache => {
+//             caches.match(e.request.url)
+//         }
+//     )
+//     e.respondWith(respuesta);
+// })
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
