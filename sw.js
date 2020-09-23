336e763adfd174b96ba51c903de6cf5d5ac6823f
fedecascardo <fedecasca@gmail.com>
2020-09-23 19:15:13 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,7 +7,13 @@ this.addEventListener('install', function(event) {
             'https://fedecascardo.github.io/',
             'https://fedecascardo.github.io/favicon.ico',
             'https://fedecascardo.github.io/manifest.json',
-            'https://fedecascardo.github.io/js/main.js'
+            'https://fedecascardo.github.io/js/main.js',
+            'css/main.css',
+            'index.html',
+            '/',
+            'favicon.ico',
+            'manifest.json',
+            'js/main.js'
         ]);
     })
   );
@@ -23,12 +29,14 @@ self.addEventListener('activate', e=> {
 self.addEventListener('fetch', e=>{
     //Responder priorizando la caché por sobre la red - cache first
     //Intercepta una petición http
-    console.log('SW fetch');  
+    //console.log('SW fetch');  
     //let request =  e.request;
     //console.log(request.url)
     //let response;
     //Esto sería el comportamiento por defecto. Responder con e.request
     //e.respondWith(fetch(e.request));
+    alert("Se dispara fetch")
+    console.log(e.request.url)
     const respuesta = caches.open('CACHE_APP_LOTES').then(
         cache => {
             caches.match(e.request.url)
